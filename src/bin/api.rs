272e74ad3b42e9adfd894113127bcb2448a28554
ragnyll <ragnyll@gallowzhumour.dev>
2021-08-08 23:35:10 -0500

    have an example row

--- src/bin/api.rs
+++ src/bin/api.rs
@@ -1,3 +1,34 @@
+extern crate diesel;
+extern crate schoolapi;
+
+use self::schoolapi::*;
+use self::models::*;
+use self::diesel::prelude::*;
+
 fn main() {
-    println!("writing an api")
+    use schoolapi::schema::school::dsl::*;
+    use schoolapi::schema::school;
+
+    let connection = establish_connection();
+
+    let new_school = NewSchool {
+        name: "street styles",
+    };
+
+    // diesel::insert_into(school::table)
+        // .values(&new_school)
+        // .get_result::<School>(&connection)
+        // .expect("Error saving new post");
+
+    let results = school
+        .limit(5)
+        .load::<School>(&connection)
+        .expect("Error loading school");
+
+    println!("Displaying {} schools", results.len());
+    for result in results {
+        println!("{}", result.id);
+        println!("----------\n");
+        println!("{}", result.name);
+    }
 }
---
+++ src/models.rs
@@ -0,0 +1,13 @@
+use super::schema::school;
+
+#[derive(Queryable, Debug)]
+pub struct School {
+    pub id: i32,
+    pub name: String,
+}
+
+#[derive(Insertable)]
+#[table_name="school"]
+pub struct NewSchool<'a> {
+    pub name: &'a str,
+}
