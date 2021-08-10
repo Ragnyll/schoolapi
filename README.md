5eab6424741b168703eb91a527bbe028d2e52ab1
ragnyll <ragnyll@gallowzhumour.dev>
2021-08-10 00:07:35 -0500

    created a single endpoint

---
+++ README.md
@@ -0,0 +1,5 @@
+# Schooled
+## Initialization
+```
+export DATABASE_URL=postgres://ragnyll:878neveragain@localhost/school_db
+```
--- src/bin/api.rs
+++ src/bin/api.rs
@@ -1,34 +1,56 @@
-extern crate diesel;
-extern crate schoolapi;
+#[macro_use]
+extern crate rocket;
 
-use self::schoolapi::*;
-use self::models::*;
+use schoolapi::schema::school::dsl::*;
 use self::diesel::prelude::*;
 
-fn main() {
-    use schoolapi::schema::school::dsl::*;
-    use schoolapi::schema::school;
+use rocket_sync_db_pools::{diesel, database};
+use schoolapi::schema::school::dsl::*;
+use schoolapi::schema::school;
+use schoolapi::models::School;
 
-    let connection = establish_connection();
+#[database("postgresql_schooldb")]
+struct SchoolDbConn(diesel::PgConnection);
 
-    let new_school = NewSchool {
-        name: "street styles",
-    };
+use rocket_sync_db_pools::diesel::RunQueryDsl;
+
+#[get("/schools")]
+async fn all_schools(conn: SchoolDbConn) -> &'static str {
+    let content = conn.run(|c| school.load::<School>(c)).await.expect("couldnt load");
+    println!("{:?}", content);
+
+    // let connection = establish_connection();
+
+    // let new_school = NewSchool {
+    // name: "street styles",
+    // };
 
     // diesel::insert_into(school::table)
-        // .values(&new_school)
-        // .get_result::<School>(&connection)
-        // .expect("Error saving new post");
+    // .values(&new_school)
+    // .get_result::<School>(&connection)
+    // .expect("Error saving new post");
 
-    let results = school
-        .limit(5)
-        .load::<School>(&connection)
-        .expect("Error loading school");
+    // let results = school
+        // .limit(5)
+        // .load::<School>(&connection)
+        // .expect("Error loading school");
 
-    println!("Displaying {} schools", results.len());
-    for result in results {
-        println!("{}", result.id);
-        println!("----------\n");
-        println!("{}", result.name);
-    }
+    // println!("Displaying {} schools", results.len());
+
+    // for result in results {
+        // println!("{}", result.id);
+        // println!("----------\n");
+        // println!("{}", result.name);
+    // }
+    "Hello world"
+}
+
+#[rocket::main]
+async fn main() {
+    rocket::build()
+        .attach(SchoolDbConn::fairing())
+        .mount("/", routes![all_schools])
+        .launch()
+        .await
+        .expect("Unable to start server");
 }
